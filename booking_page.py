3f2c9e20949b0a7ad8911634b5e74ee04f8383b2
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-07-10 01:31:02 +0700

    Update booking_page.py

--- booking_page.py
+++ booking_page.py
@@ -73,7 +73,7 @@ def appointment(PatientID):
             if doctor_info['Ảnh'].values[0] != "" :
                 st.image(doctor_info['Ảnh'].values[0], width= 250)
             else:
-                unknown_doctor = "D:/Users/User/Downloads/Unknown_person.jpg"
+                unknown_doctor = "Unknown_person.jpg"
                 st.image(unknown_doctor, width= 250)
         with col_2:
             st.subheader(doctor_name)
